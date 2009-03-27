e4f5c256a40def828624cc3ce28cd305fcb2ddf8
Uli Köhler <ulikoehler@online.de>
2009-03-27 20:44:48 +0100

    Added readVal() code

--- xraysim/3dio.hpp
+++ xraysim/3dio.hpp
@@ -9,10 +9,44 @@
 #define	_3DIO_HPP
 
 #include <string>
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+
+#define FIELD_DELIM ',' //Character that separates 2 values
 
 using namespace std;
+using namespace boost;
 
-struct data
+/**
+ * Reads digits from the supplied input stream until FIELD_DELIM (constant)
+ * is read and returns the digits converted into a number
+ * \param in The input stream to read from
+ * \return The converted number
+ */
+template<class T>
+inline T
+readVal (std::istream& in)
+{
+    static string buffer; //Buffers the digits read from in
+    static char c;
+    buffer = "";
+
+    while (true) //Break condition: return...
+        {
+            in.get (c);
+
+            //Check if the char read in is an number
+            //(Checks if the character is in the ASCII digit range
+            if (c >= '0' && c <= '9')
+                {
+                    buffer += c;
+                }
+            else if (c == FIELD_DELIM) //Last character of the field
+                {
+                    return boost::lexical_cast<T>(buffer);
+                }
+            }
+}
 
 #endif	/* _3DIO_HPP */
 
