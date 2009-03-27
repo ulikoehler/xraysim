3495b6e39cdfdd2d55cf49d73b4b1fede11c9299
Uli Köhler <ulikoehler@online.de>
2009-03-27 20:22:28 +0100

    Merged 3D IO Test hpp into the main test file (will use monolithic test architecture)

--- xraysim/test/testmain.cpp
+++ xraysim/test/testmain.cpp
@@ -1,6 +1,22 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE XRaySim Test
 #include <boost/test/included/unit_test.hpp>
-#include "3diotest.hpp"
+#include <iostream>
+#include <sstream>
 
+#include "../3dio.hpp"
 
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE(IOTestSuite)
+        
+
+BOOST_AUTO_TEST_CASE(TestBasicFieldParsing)
+{
+    BOOST_TEST_MESSAGE("Testing readVal(istream&)");
+    stringstream ss(stringstream::in | stringstream::out);
+    ss << "5,3,5";
+    BOOST_CHECK_EQUAL(readVal<int>(ss), 5);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
