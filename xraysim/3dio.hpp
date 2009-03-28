a76e7d60d7dcd296ac6daf2787fe7847784b66c0
Uli Köhler <ulikoehler@online.de>
2009-03-28 18:13:27 +0100

    Fixed bugs, all tests work now. TODO: Move files into src directory

--- xraysim/3dio.hpp
+++ xraysim/3dio.hpp
@@ -16,10 +16,21 @@
 #include "include.hpp"
 
 /**
+ * Checks if a given character is either CR or LF
+ * \return A bool indication whether the gived character is a newline character
+ * \param c The character to test
+ */
+inline bool
+isnewline (char c)
+{
+    return (c == '\n' || c == '\f');
+}
+
+/**
  * Reads digits from the supplied input stream until FIELD_DELIM (constant)
  * is read and returns the digits converted into a number
  * \param in The input stream to read from
- * \return The converted number
+ * \return The converted unsigned integer
  */
 inline uint
 readNextVal (std::istream& in)
@@ -28,22 +39,27 @@ readNextVal (std::istream& in)
     static char c;
     buffer = "";
 
-    while (true) //Break condition: return...
+    while (true) //Break condition: return
         {
             in.get (c);
 
-            //Check if the char read in is an number
-            //(Checks if the character is in the ASCII digit range
-            if (c >= '0' && c <= '9')
+            //Check if the character read in is an number
+            if (isdigit (c))
                 {
                     buffer += c;
                 }
-            else if (c == FIELD_DELIM) //Last character of the field
+                //Break if the field delimiter or a newline character is read
+            else if (c == FIELD_DELIM || isnewline (c)) //Field delimiter declared in include.hpp
                 {
-                    return boost::lexical_cast<uint>(buffer);
+                    //Don't return if there were no digits before the first delimiter or newline character
+                    if (buffer.length () > 0)
+                        {
+                            return boost::lexical_cast<uint > (buffer);
+                        }
                 }
-            }
-}
+            //else: continue
+        }
+} 
 
 /**
  * Reads in a multi-dimensional array
@@ -53,17 +69,18 @@ readNextVal (std::istream& in)
  * \param z The z extent
  * \param in The istream to read the data from
  */
-inline matrix3d readMatrix3d(uint x, uint y, uint z, std::istream& in)
+inline matrix3d
+readMatrix3d (uint x, uint y, uint z, std::istream& in)
 {
-    matrix3d retMatrix(boost::extents[x][y][z]);
+    matrix3d retMatrix (boost::extents[x][y][z]);
 
-    for(int ix = 0; ix < x; ix++)
+    for (int ix = 0; ix < x; ix++)
         {
-            for(int iy = 0; iy < y; iy++)
+            for (int iy = 0; iy < y; iy++)
                 {
-                    for(int iz = 0; iz < z; iz++)
+                    for (int iz = 0; iz < z; iz++)
                         {
-                            retMatrix[ix][iy][iz] = readNextVal<uint32_t>(in);
+                            retMatrix[ix][iy][iz] = readNextVal (in);
                         }
                 }
         }
@@ -75,12 +92,13 @@ inline matrix3d readMatrix3d(uint x, uint y, uint z, std::istream& in)
  * See data-spec.txt for format specification
  * (Reads in the dimensions and the uses readMatrix3d() to read in the data)d
  */
-inline matrix3d readMatrix(std::istream& in)
+inline matrix3d
+readMatrix (std::istream& in)
 {
     //Read in the dimensions
-    uint xdim = readNextVal<uint>(in);
-    uint ydim = readNextVal<uint>(in);
-    uint zdim = readNextVal<uint>(in);
+    uint xdim = readNextVal (in);
+    uint ydim = readNextVal (in);
+    uint zdim = readNextVal (in);
     //Read in the data
     return readMatrix3d (xdim, ydim, zdim, in);
 }
--- xraysim/test/testmain.cpp
+++ xraysim/test/testmain.cpp
@@ -14,40 +14,71 @@ using namespace std;
 using namespace boost;
 using namespace boost::unit_test;
 
-BOOST_AUTO_TEST_SUITE(IOTestSuite)
+BOOST_AUTO_TEST_SUITE (IOTestSuite)
 
 
-BOOST_AUTO_TEST_CASE(init)
+BOOST_AUTO_TEST_CASE (init)
 {
-    unit_test_log.set_threshold_level( log_messages );
+    unit_test_log.set_threshold_level (log_messages);
 }
 
 /**
  * Tests readNextVal()
  * Test parsing of streams into numbers
  */
-BOOST_AUTO_TEST_CASE(TestReadNextVal)
+BOOST_AUTO_TEST_CASE (TestReadNextVal)
 {
-    BOOST_TEST_MESSAGE("Testing readNextVal()");
-    stringstream ss(stringstream::in | stringstream::out); //Buffers the comma-separated data
+    BOOST_TEST_MESSAGE ("Testing readNextVal()");
+    stringstream ss (stringstream::in | stringstream::out); //Buffers the comma-separated data
 
     /**
      * Generate TEST_NUMBERS integers and save them in an array and the stringstream
      */
-    BOOST_TEST_MESSAGE("    Testing integer parsing");
-    mt19937 intRng(time(0)); //Init a MT19937 PRNG (warning: low entropy seed)
+    BOOST_TEST_MESSAGE ("    Testing integer parsing");
+    mt19937 intRng (time (0)); //Init a MT19937 PRNG (warning: low entropy seed)
     uint32_t intNumbers[TEST_NUMBERS];
-    for(int i = 0; i < TEST_NUMBERS; i++)
+    for (int i = 0; i < TEST_NUMBERS; i++)
         {
-            uint32_t rand = intRng(); //Generate a random number
+            uint32_t rand = intRng (); //Generate a random number
             intNumbers[i] = rand;
             ss << rand << ',';
         }
     //Check if the parsing leads to the same results
-    for(int i = 0; i < TEST_NUMBERS; i++)
+    for (int i = 0; i < TEST_NUMBERS; i++)
         {
             //readNextVal parses the next number each iteration
-            BOOST_CHECK_EQUAL(readNextVal<uint32_t>(ss), intNumbers[i]);
+            //Test if it's equal to the number saved in the array
+            BOOST_CHECK_EQUAL (readNextVal (ss), intNumbers[i]);
+        }
+
+    /**
+     * Do the same with 'noise characters)
+     */
+    BOOST_TEST_MESSAGE ("    Testing noisy integer parsing");
+    for (int i = 0; i < TEST_NUMBERS; i++)
+        {
+            uint32_t rand = intRng (); //Generate a random number
+            intNumbers[i] = rand;
+            //Add a noise character
+            //Generate characters until a character is not a number
+            char noise;
+            while (true) //Generate characters
+                {
+                    noise = intRng () % 256;
+                    if (!isdigit(noise)) //Break if the generated character is not a number
+                        {
+                            break;
+                        } 
+                }
+            ss << noise << rand << ',';
+        }
+    //Check if the parsing leads to the same results
+    for (int i = 0; i < TEST_NUMBERS; i++)
+        {
+            //readNextVal parses the next number each iteration
+            //Test if it's equal to the number saved in the array
+            //Throws a bad_lexical_cast if the noise isn't recognized
+            BOOST_CHECK_EQUAL (readNextVal (ss), intNumbers[i]);
         }
 }
 
@@ -55,23 +86,23 @@ BOOST_AUTO_TEST_CASE(TestReadNextVal)
  * Tests readMatrix3d()
  * Test reading of 3d matrices
  */
-BOOST_AUTO_TEST_CASE(TestReadMatrix3d)
+BOOST_AUTO_TEST_CASE (TestReadMatrix3d)
 {
-    stringstream ss(stringstream::in | stringstream::out); //Buffers the comma-separated integers
-    mt19937 intRng(time(0)); //Init a MT19937 PRNG (warning: low entropy seed)
+    stringstream ss (stringstream::in | stringstream::out); //Buffers the comma-separated integers
+    mt19937 intRng (time (0)); //Init a MT19937 PRNG (warning: low entropy seed)
 
-    BOOST_TEST_MESSAGE("Testing readMatrix3d()");
+    BOOST_TEST_MESSAGE ("Testing readMatrix3d()");
 
     //Generate a random 10,9,8 matrix
-    matrix3d randMatrix(boost::extents[10][9][8]);
+    matrix3d randMatrix (boost::extents[10][9][8]);
 
-    for(int ix = 0; ix < 10; ix++)
+    for (int ix = 0; ix < 10; ix++)
         {
-            for(int iy = 0; iy < 9; iy++)
+            for (int iy = 0; iy < 9; iy++)
                 {
-                    for(int iz = 0; iz < 8; iz++)
+                    for (int iz = 0; iz < 8; iz++)
                         {
-                            uint32_t rand = intRng();
+                            uint32_t rand = intRng ();
                             randMatrix[ix][iy][iz] = rand;
                             ss << rand << ',';
                         }
@@ -79,35 +110,35 @@ BOOST_AUTO_TEST_CASE(TestReadMatrix3d)
         }
 
     //Re-read the matrix from the stringstream
-    matrix3d rereadMatrix = readMatrix3d(10, 9, 8, ss);
+    matrix3d rereadMatrix = readMatrix3d (10, 9, 8, ss);
 
     //Check the equality of the two matrices
-    BOOST_CHECK(randMatrix == rereadMatrix);
+    BOOST_CHECK (randMatrix == rereadMatrix);
 }
 
 /**
  * Tests readMatrix
  * Test reading of data files
  */
-BOOST_AUTO_TEST_CASE(TestReadDataFile)
+BOOST_AUTO_TEST_CASE (TestReadDataFile)
 {
-    stringstream ss(stringstream::in | stringstream::out); //Buffers the comma-separated integers
-    mt19937 intRng(time(0)); //Init a MT19937 PRNG (warning: low entropy seed)
+    stringstream ss (stringstream::in | stringstream::out); //Buffers the comma-separated integers
+    mt19937 intRng (time (0)); //Init a MT19937 PRNG (warning: low entropy seed)
 
-    BOOST_TEST_MESSAGE("Testing readMatrix()");
+    BOOST_TEST_MESSAGE ("Testing readMatrix()");
 
     //Generate a random 10,9,8 matrix
-    matrix3d randMatrix(boost::extents[10][9][8]);
+    matrix3d randMatrix (boost::extents[10][9][8]);
 
     ss << 10 << ',' << 9 << ',' << 8 << endl; //Prints out the dimenstion
 
-    for(int ix = 0; ix < 10; ix++)
+    for (int ix = 0; ix < 10; ix++)
         {
-            for(int iy = 0; iy < 9; iy++)
+            for (int iy = 0; iy < 9; iy++)
                 {
-                    for(int iz = 0; iz < 8; iz++)
+                    for (int iz = 0; iz < 8; iz++)
                         {
-                            uint32_t rand = intRng();
+                            uint32_t rand = intRng ();
                             randMatrix[ix][iy][iz] = rand;
                             ss << rand << ',';
                         }
@@ -115,10 +146,10 @@ BOOST_AUTO_TEST_CASE(TestReadDataFile)
         }
 
     //Re-read the matrix from the stringstream
-    matrix3d rereadMatrix = readMatrix(ss);
+    matrix3d rereadMatrix = readMatrix (ss);
 
     //Check the equality of the two matrices
-    BOOST_CHECK(randMatrix == rereadMatrix);
+    BOOST_CHECK (randMatrix == rereadMatrix);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END ()
