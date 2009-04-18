bdf02f9df2b2d81e4a9dc841ed1c61af3c020ecd
Uli Köhler <ulikoehler@online.de>
2009-04-18 16:03:41 +0200

    Added some non-working zlib draft code

--- backprojector/src/matrix.cpp
+++ backprojector/src/matrix.cpp
@@ -47,6 +47,73 @@ Matrix2d::writeTo (std::ostream& out)
         }
 }
 
+///Gzip code
+#define CHUNKSIZE 16384
+
+
+int Matrix2d::ReadFromGz (std::istream& in)
+{
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+    z_stream strm;
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    int ret = inflateInit (&strm);
+    if (ret != Z_OK)
+        {
+            cerr << "Error in GZ decompression";
+        }
+
+    /* decompress until deflate stream ends or end of file */
+    do
+        {
+
+            strm.avail_in = in.read();
+            if (!in.good())
+                {
+                    (void) inflateEnd (&strm);
+                    return Z_ERRNO;
+                }
+            if (strm.avail_in == 0) break;
+            strm.next_in = in;
+
+            /* run inflate() on input until output buffer not full */
+            do
+                {
+                    strm.avail_out = CHUNK;
+                    strm.next_out = out;
+                    ret = inflate (&strm, Z_NO_FLUSH);
+                    assert (ret != Z_STREAM_ERROR); /* state not clobbered */
+                    switch (ret)
+                        {
+                        case Z_NEED_DICT:
+                            ret = Z_DATA_ERROR; /* and fall through */
+                        case Z_DATA_ERROR:
+                        case Z_MEM_ERROR:
+                            (void) inflateEnd (&strm);
+                            return ret;
+                        }
+                    have = CHUNK - strm.avail_out;
+                    if (fwrite (out, 1, have, dest) != have || ferror (dest))
+                        {
+                            (void) inflateEnd (&strm);
+                            return Z_ERRNO;
+                        }
+                }
+            while (strm.avail_out == 0);
+
+            /* done when inflate() says it's done */
+        }
+    while (ret != Z_STREAM_END);
+
+    /* clean up and return */
+    (void) inflateEnd (&strm);
+    return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+
+
+}
+
 void
 Matrix2d::ReadFromPlainStream ()
 {
