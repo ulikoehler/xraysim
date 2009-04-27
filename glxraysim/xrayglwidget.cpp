7738a1c91ce913c9414e1fe482fe3b6c82e4cc48
Uli Köhler <ulikoehler@online.de>
2009-04-27 15:53:03 +0200

    Improved performance in the textured plane drawing list by using 2d coordinates and GL_TRIANGLE_STRING instead of 3d coordinates and GL_QUADS now.

--- glxraysim/xrayglwidget.cpp
+++ glxraysim/xrayglwidget.cpp
@@ -269,11 +269,11 @@ void XRayGLWidget::initializeGL()
     drawTexturedPlaneListID = drawCubeListID + 1;
 
     glNewList(drawTexturedPlaneListID, GL_COMPILE);
-        glBegin(GL_QUADS);
-                 glTexCoord2f(0,0); glVertex3f(-1,-1,0); //lo
-                 glTexCoord2f(0,1); glVertex3f(-1,1,0); //lu
-                 glTexCoord2f(1,1); glVertex3f(1,1,0);  //ru
-                 glTexCoord2f(1,0); glVertex3f(1,-1,0); //ro
+        glBegin(GL_TRIANGLE_STRIP);
+                 glTexCoord2f(0,1); glVertex2f(-1,1); //lo
+                 glTexCoord2f(0,0); glVertex2f(-1,-1); //lu
+                 glTexCoord2f(1,1); glVertex2f(1,1);  //ru
+                 glTexCoord2f(1,0); glVertex2f(1,-1); //ro
         glEnd();
     glEndList();
 }
